fa394d607b4938a3236cd31d4e746ae8b911cc1c
ANNA_ARAGAO <anna.aragao@sou.inteli.edu.br>
2024-03-13 21:12:02 -0300

    feat: integrated front with mocked back

--- backend/quests.js
+++ backend/quests.js
@@ -37,6 +37,135 @@ let mockQuests = [
     }
   ];
 
+   const mockPlayers = [
+      {
+        "nickname": "ShadowBlade",
+        "class": "Rogue",
+        "lvl": 30,
+        "health": 250,
+        "attack": 80,
+        "defense": 40,
+        "strength": 60,
+        "intelligence": 50,
+        "charisma": 30,
+        "dexterity": 90
+      },
+      {
+        "nickname": "Stormcaster",
+        "class": "Wizard",
+        "lvl": 25,
+        "health": 180,
+        "attack": 70,
+        "defense": 30,
+        "strength": 40,
+        "intelligence": 90,
+        "charisma": 50,
+        "dexterity": 60
+      },
+      {
+        "nickname": "Ironclad",
+        "class": "Knight",
+        "lvl": 35,
+        "health": 350,
+        "attack": 60,
+        "defense": 90,
+        "strength": 80,
+        "intelligence": 40,
+        "charisma": 50,
+        "dexterity": 50
+      },
+      {
+        "nickname": "Swiftshadow",
+        "class": "Assassin",
+        "lvl": 28,
+        "health": 200,
+        "attack": 85,
+        "defense": 35,
+        "strength": 50,
+        "intelligence": 40,
+        "charisma": 40,
+        "dexterity": 90
+      },
+      {
+        "nickname": "Flameheart",
+        "class": "Mage",
+        "lvl": 22,
+        "health": 160,
+        "attack": 75,
+        "defense": 25,
+        "strength": 30,
+        "intelligence": 85,
+        "charisma": 60,
+        "dexterity": 50
+      },
+      {
+        "nickname": "Thunderclap",
+        "class": "Barbarian",
+        "lvl": 32,
+        "health": 300,
+        "attack": 90,
+        "defense": 50,
+        "strength": 95,
+        "intelligence": 20,
+        "charisma": 30,
+        "dexterity": 40
+      },
+      {
+        "nickname": "Nightshade",
+        "class": "Ranger",
+        "lvl": 27,
+        "health": 220,
+        "attack": 80,
+        "defense": 45,
+        "strength": 65,
+        "intelligence": 50,
+        "charisma": 50,
+        "dexterity": 80
+      },
+      {
+        "nickname": "Frostbite",
+        "class": "Sorcerer",
+        "lvl": 23,
+        "health": 170,
+        "attack": 80,
+        "defense": 30,
+        "strength": 35,
+        "intelligence": 85,
+        "charisma": 55,
+        "dexterity": 60
+      },
+      {
+        "nickname": "Steelwind",
+        "class": "Paladin",
+        "lvl": 29,
+        "health": 280,
+        "attack": 70,
+        "defense": 80,
+        "strength": 75,
+        "intelligence": 45,
+        "charisma": 70,
+        "dexterity": 45
+      },
+      {
+        "nickname": "Shadowfang",
+        "class": "Ninja",
+        "lvl": 31,
+        "health": 240,
+        "attack": 95,
+        "defense": 40,
+        "strength": 60,
+        "intelligence": 40,
+        "charisma": 40,
+        "dexterity": 95
+      }
+    ];
+    
+
+// get/players
+router.get('/players', (req, res) => {
+  res.json(mockPlayers)
+})
+
 // get /quests
 router.get('/quests', (req, res) => {
     res.json(mockQuests)
@@ -52,7 +181,6 @@ router.get('/quest/:id', (req, res) => {
 
 // put/quests/id
 router.put('/quest/:id', (req, res) => {
-
     const id = req.params.id
     mockQuests.forEach(q => {
         if (q.id == id) {
